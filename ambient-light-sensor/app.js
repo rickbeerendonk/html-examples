df009fba534f2401d359b1228bd7601770d9a71e
Rick Beerendonk <rick@beerendonk.com>
2018-02-12 22:13:32 +0100

    Logical grouping comments.

--- ambient-light-sensor/app.js
+++ ambient-light-sensor/app.js
@@ -3,17 +3,19 @@
 
 /*
 
-API:
+Specification:
 https://w3c.github.io/ambient-light/
 https://github.com/w3c/ambient-light
 
 Documentation:
-https://developer.microsoft.com/en-us/microsoft-edge/platform/status/ambientlightsensorapi/
 https://developer.mozilla.org/en-US/docs/Web/API/AmbientLightSensor
 
 Security:
 https://blog.lukaszolejnik.com/stealing-sensitive-browser-data-with-the-w3c-ambient-light-sensor-api/
 https://github.com/w3c/ambient-light/commit/23a64553078d054a8057914e93e7e0032bf47b4c
+
+Support:
+https://developer.microsoft.com/en-us/microsoft-edge/platform/status/ambientlightsensorapi/
 
 Enable:
 chrome://flags/#enable-generic-sensor
