498d17ce541d950057edb22d4ff93af16f43388d
Rick Beerendonk <rick@beerendonk.com>
2018-03-02 10:53:14 -0500

    Add Security test link & reorder folders.

---
+++ Dialog/app.js
@@ -0,0 +1,19 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/*
+
+Specification:
+https://html.spec.whatwg.org/multipage/interactive-elements.html#the-dialog-element
+
+Documentation:
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog
+
+Support:
+https://developer.microsoft.com/en-us/microsoft-edge/platform/status/dialogelementformodals/
+https://caniuse.com/#feat=dialog
+
+Enable:
+Supported in Firefox by enabling "dom.dialog_element.enabled" in about:config
+
+*/
