0371be87c7735227cb2c2c183d76296b3c9d67d1
Rick Beerendonk <rick@beerendonk.com>
2020-01-09 14:24:01 +0100

    Add copyright.

--- Storybook/02. Dynamic loading/.storybook/addons.js
+++ Storybook/02. Dynamic loading/.storybook/addons.js
@@ -1,1 +1,4 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
 import '@storybook/addon-actions/register';
--- Storybook/02. Dynamic loading/.storybook/config.js
+++ Storybook/02. Dynamic loading/.storybook/config.js
@@ -1,3 +1,6 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
 import { configure } from '@storybook/html';
 
 const req = require.context('../stories', true, /\.stories\.js$/);
