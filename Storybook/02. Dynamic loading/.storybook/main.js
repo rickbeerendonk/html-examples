3cb25158ae012b86a0d7f56de9928ed5bb2e42e2
Rick Beerendonk <rick@beerendonk.com>
2020-01-14 08:11:55 +0100

    Upgrade to Storybook v5.3.

---
+++ Storybook/02. Dynamic loading/.storybook/main.js
@@ -0,0 +1,7 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+module.exports = {
+  stories: ['../stories/**/*.stories.js'],
+  addons: ['@storybook/addon-actions/register']
+};
