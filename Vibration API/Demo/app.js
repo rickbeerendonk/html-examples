0479fdd2601796e3490de227f1a6ae808b76cecd
Rick Beerendonk <rick@beerendonk.com>
2020-07-04 07:06:04 +0200

    Add cancel vibration to the example.

--- Vibration API/Demo/app.js
+++ Vibration API/Demo/app.js
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2019 Rick Beerendonk          !*/
 
-function vibrateHandler() {
+function startVibrateHandler() {
   // Vibrate 3 times with pauses of 100 milliseconds.
   // The vibrations should be longer each time.
   window.navigator.vibrate([100, 100, 300, 100, 500]);
@@ -9,5 +9,12 @@ function vibrateHandler() {
   console.log('Vibrated');
 }
 
-const elem = document.getElementById('vibrate');
-elem.addEventListener('click', vibrateHandler);
+function stopVibrateHandler() {
+  // Cancels any currently ongoing vibration.
+  window.navigator.vibrate(0);
+
+  console.log('Vibration stopped');
+}
+
+document.querySelector('#start').addEventListener('click', startVibrateHandler);
+document.querySelector('#stop').addEventListener('click', stopVibrateHandler);
